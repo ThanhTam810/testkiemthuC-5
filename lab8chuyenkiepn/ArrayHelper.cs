bdafe1f8cce347bb5195bc7a80c6decba20d8bf1
Tâm Thanh <lebinhnhi113@gmail.com>
2025-10-15 14:14:42 +0700

    Thêm toàn bộ file dự án Lab8 chuyển kiếp

---
+++ lab8chuyenkiepn/ArrayHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ArrayLib
+{
+    public static class ArrayHelper
+    {
+        public static int FindMax(int[] arr)
+        {
+            if (arr == null || arr.Length == 0)
+                throw new ArgumentException("Array must not be empty");
+            int max = arr[0];
+            foreach (var n in arr)
+                if (n > max) max = n;
+            return max;
+        }
+
+        public static int FindMin(int[] arr)
+        {
+            if (arr == null || arr.Length == 0)
+                throw new ArgumentException("Array must not be empty");
+            int min = arr[0];
+            foreach (var n in arr)
+                if (n < min) min = n;
+            return min;
+        }
+    }
+}
