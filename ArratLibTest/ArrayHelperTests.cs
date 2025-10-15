d5eaba87e7539a8f23da03f8a4b7247333b6e8f6
Tâm Thanh <lebinhnhi113@gmail.com>
2025-10-15 14:14:42 +0700

    Thêm toàn bộ file dự án Lab8 chuyển kiếp

---
+++ ArratLibTest/ArrayHelperTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ArrayLib;
+using System;
+
+namespace ArrayLibTest
+{
+    [TestClass]
+    public class ArrayHelperTests
+    {
+        [TestMethod]
+        public void FindMax_ReturnsCorrectValue()
+        {
+            int[] arr = { 1, 5, 3, 9, 2 };
+            Assert.AreEqual(9, ArrayHelper.FindMax(arr));
+        }
+
+        [TestMethod]
+        public void FindMin_ReturnsCorrectValue()
+        {
+            int[] arr = { 1, 5, 3, 9, 2 };
+            Assert.AreEqual(1, ArrayHelper.FindMin(arr));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void FindMax_EmptyArray_ThrowsException()
+        {
+            ArrayHelper.FindMax(new int[] { });
+        }
+    }
+}
